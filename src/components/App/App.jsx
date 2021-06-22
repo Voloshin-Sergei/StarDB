b85ca4438bdce08452260964cfa6a2855d57b817
Voloshin_Sergey <4voloshin@gmail.com>
2021-06-22 22:09:45 +0300

    feat: add Person component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,19 @@
 import Header from '../Header/Header';
 import Random from '../Random/Random';
 import ItemList from '../ItemList/ItemList';
+import Person from '../Person/Person';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Random />
-      <div className="row mb2">
+      <div className="mb2">
         <div className="col-md-6">
           <ItemList />
+        </div>
+        <div className="col-md-6">
+          <Person />
         </div>
       </div>
     </div>
