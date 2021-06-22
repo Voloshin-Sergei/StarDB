e484e2bc8755438d13af7c315cfa4de23399e459
Voloshin_Sergey <4voloshin@gmail.com>
2021-06-22 21:57:45 +0300

    feat: add ItemList component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,17 @@
 import Header from '../Header/Header';
 import Random from '../Random/Random';
+import ItemList from '../ItemList/ItemList';
 
 const App = () => {
   return (
     <div>
       <Header />
       <Random />
+      <div className="row mb2">
+        <div className="col-md-6">
+          <ItemList />
+        </div>
+      </div>
     </div>
   );
 };
